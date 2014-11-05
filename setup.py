a0a1fd64997db2a5790691704807a78dff2ec8fb
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2014-11-05 10:43:37 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='twitter-index',
+    description='Index home feed and favorite tweets.',
+    version='0.1.0',
+    author='Sviatoslav Abakumov',
+    author_email='dust.harvesting@gmail.com',
+    url='https://bitbucket.org/Perlence/twitter-index',
+    platforms=['MacOS X', 'Unix', 'POSIX'],
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'arrow',
+        'gevent',
+        'mongokit',
+        'schedule',
+        'whoosh',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
