e665346e34ff1dd3481fc3c0351ed21fc093d6da
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2014-11-06 14:46:16 +0300

    Move assets to its own module

---
+++ twitterindex/assets.py
@@ -0,0 +1,40 @@
+from os import path
+
+from flask import send_from_directory
+from flask.ext.assets import Environment, Bundle
+
+
+def init_app(app):
+    env = Environment(app)
+    env.load_path = [
+        path.join(path.dirname(__file__), 'scss'),
+        path.join(path.dirname(__file__), 'coffee'),
+        path.join(path.dirname(__file__), 'bower_components'),
+    ]
+    js_bundle = Bundle(
+        Bundle(
+            'jquery/dist/jquery.js',
+            'bootstrap-sass-official/assets/javascripts/bootstrap.js',
+            output='js_requirements.js'),
+        Bundle(
+            'index.coffee',
+            filters=['coffeescript'],
+            output='js_index.js'))
+    css_bundle = Bundle(
+        'all.scss',
+        filters=['scss', 'autoprefixer'],
+        output='css_all.css')
+    env.config['sass_load_paths'] = [
+        path.join(
+            path.dirname(__file__),
+            'bower_components/bootstrap-sass-official/assets/stylesheets/'),
+    ]
+    env.register('js_all', js_bundle)
+    env.register('css_all', css_bundle)
+
+    @app.route('/static/fonts/<path:fontname>')
+    def static_fonts(fontname):
+        directory = path.join(
+            path.dirname(__file__),
+            'bower_components/bootstrap-sass-official/assets/fonts/bootstrap')
+        return send_from_directory(directory, fontname)
